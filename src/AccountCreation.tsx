9818c2d6d278bd1efed30ab9b3e5bee452056329
googed2much <justinasbelskis@gmail.com>
2026-04-23 11:24:57 +0300

    Added alert and close functions after saving user.

--- src/AccountCreation.tsx
+++ src/AccountCreation.tsx
@@ -45,6 +45,13 @@ function AccountCreation({ close }: { close: () => void }){
 
     async function saveNewUser(){
         const response = await fetch(`/api/users/create`, {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(newUser), credentials: "include"});
+        if(response.ok){
+            alert("Issaugotas naudotojas!")
+            close()
+        }
+        else{
+            alert("Nepavyko issaugoti naudotojo")
+        }
     }
 
     return (
